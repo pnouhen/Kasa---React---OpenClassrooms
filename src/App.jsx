4dc05a10d8112ffc8dd61538cdbb6dbbd3381cbf
Pierre Nouhen <pierre.nouhen@orange.fr>
2025-02-20 14:38:17 +0100

    Firt commit + Header

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './styles/index.scss'
+import Header from './components/Header.jsx'
+
+function App() {
+  return (
+  <Header></Header>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import "../styles/header.scss"
+
+export default function Header() {
+  return (
+    <header>
+      <img src="./src/assets/logo/logo-rouge.png"></img>
+      <nav>
+          <a href="" id="accueil">Accueil</a>
+          <a href="" id="a-propos">A Propos</a>
+      </nav>
+    </header>
+  );
+}
