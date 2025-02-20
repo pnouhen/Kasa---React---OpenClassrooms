1fc2363501b7a1bfbb2be033b336683c3b6cd208
Pierre Nouhen <pierre.nouhen@orange.fr>
2025-02-20 16:42:03 +0100

    Title fait et la section en cours

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './styles/index.scss'
+import './styles/body.scss'
 import Header from './components/Header.jsx'
+import Title from './components/Accueil.jsx'
 
 function App() {
   return (
-  <Header></Header>
+    <>
+    <Header></Header>
+  <Title></Title>
+    </>
+  
   )
 }
 
---
+++ src/components/Accueil.jsx
@@ -0,0 +1,33 @@
+import "../styles/index.scss";
+
+export default function Title() {
+  return (
+    <>
+      <div className="title">
+        <img src="./src/assets/img/accueil.png" alt="Picture title" />
+        <h1>Chez vous, partout et ailleurs</h1>
+      </div>
+      <section>
+      <article>
+        <img src="./src/assets/img/card.png" alt="Picture article"></img>
+        <h2>Titre de la <br></br> location</h2>
+      </article>
+      <article>
+        <h2>Titre de la <br></br> location</h2>
+      </article>
+      <article>
+        <h2>Titre de la <br></br> location</h2>
+      </article>
+      <article>
+        <h2>Titre de la <br></br> location</h2>
+      </article>
+      <article>
+        <h2>Titre de la <br></br> location</h2>
+      </article>
+      <article>
+        <h2>Titre de la <br></br> location</h2>
+      </article>
+      </section>
+    </>
+  );
+}
