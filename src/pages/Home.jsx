975ce93dbf2ddfd2aec861e4ec2a2c946e50210a
Pierre Nouhen <pierre.nouhen@orange.fr>
2025-04-27 12:03:05 +0200

    Modification du if

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,21 +4,23 @@ import Header from "../components/Header/Header.jsx";
 import Banner from "../components/Banner/Banner.jsx";
 
 import Footer from "../components/Footer/Footer.jsx";
-import Cards from "../components/Cards/Cards.jsx"
+import Cards from "../components/Cards/Cards.jsx";
 import img from "../assets/img/home.webp";
 
 export default function Home() {
   const [data, setData] = useState([]);
-  const [loading, setLoading]=useState(true)
+  const [loading, setLoading] = useState(true);
 
-useEffect(() => {
-  fetchData()
-      .then(data => setData(data))
-      .catch(err => console.error(err))
+  useEffect(() => {
+    fetchData()
+      .then((data) => setData(data))
+      .catch((err) => console.error(err))
       .finally(() => setLoading(false));
-})
+  });
 
-if (loading) return <p>Chargement...</p>
+  if (loading) {
+    return <p>Chargement...</p>;
+  }
 
   return (
     <>
@@ -29,11 +31,10 @@ if (loading) return <p>Chargement...</p>
         title="Chez vous, partout et ailleurs"
       ></Banner>
       <section className="section-cards">
-      {data.map((item) => (
-          <Cards item={item} key={item.id}>
-          </Cards>
+        {data.map((item) => (
+          <Cards item={item} key={item.id}></Cards>
         ))}
-    </section>
+      </section>
       <Footer />
     </>
   );
